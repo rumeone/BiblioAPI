c13110f0e444dcfeacb5c8fdc9d757bf443accef
rumeone <yuaroslav_29@mail.ru>
2022-12-14 00:04:36 +0700

    Added model from book and reader

--- project/src/index.js
+++ project/src/index.js
@@ -1,11 +1,14 @@
 require('dotenv').config();
 const express = require('express');
+const sequelize = require('./db');
 const app = express();
 
 app.use(express.json());
 
 const start = async() => {
     try {
+        await sequelize.authenticate();
+        await sequelize.sync({alter: true})
         app.listen(process.env.PORT, () => {
             console.log(`Server has been started on port ${process.env.PORT}`);
         });
---
+++ project/src/models/models.js
@@ -0,0 +1,23 @@
+const {DataTypes} = require('sequelize');
+const sequelize = require('../db');
+
+
+const Book = sequelize.define('book', {
+    id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
+    title: {type: DataTypes.STRING, allowNull: false},
+    author: {type: DataTypes.STRING, allowNull: false},
+    vendorCode: {type: DataTypes.INTEGER, allowNull: false},
+    year: {type: DataTypes.INTEGER, allowNull: false},
+    numberOfCopies: {type: DataTypes.INTEGER}
+});
+
+const Reader = sequelize.define('reader', {
+    id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
+    fullName: {type: DataTypes.STRING, allowNull: false},
+    birth: {type: DataTypes.DATEONLY, allowNull: false}
+});
+
+Reader.hasMany(Book);
+Book.belongsTo(Reader);
+
+module.exports = {Book, Reader};
