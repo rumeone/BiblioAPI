18f44984cea4159fe97fe028b61d7008d6a7e6d8
rumeone <yuaroslav_29@mail.ru>
2022-12-16 22:10:39 +0700

    Micro fix

--- project/src/routes/readerRouter.js
+++ project/src/routes/readerRouter.js
@@ -7,7 +7,7 @@ router.put('/update', readerController.changeReader);
 router.delete('/delete', readerController.deleteReader);
 router.post('/issueBook', readerController.issueBook);
 router.post('/returnBook', readerController.returnBook);
-router.get('/getData:id', readerController.getDataById);
+router.get('/getData/:id', readerController.getDataById);
 router.post('/getDataByName', readerController.getDataByName);
 
 
